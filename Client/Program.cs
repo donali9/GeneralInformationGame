ee8c44231dbed77cf0fdf2548c937fb5b64ac930
Ali Moghaddam <34958849+donali9@users.noreply.github.com>
2024-05-01 20:40:31 +0330

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,8 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://87.248.130.30:8280/") });
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7218/") });
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddBlazoredLocalStorage();
